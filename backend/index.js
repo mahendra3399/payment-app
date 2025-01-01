00b836b7cea691faa9855acbb5eb10a1c26e49b4
mahendra <pvdmahendra@gmail.com>
2025-01-02 05:29:28 +0530

    initialized backend and frontend structure

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import router from "./routes/index.js";
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use("/api/v1", router);
+
+const PORT = process.env.PORT || 5000;
 
 
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server is running on port: ", PORT);
+});
---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: 3,
+        lowercase: true,
+        maxLength: 20
+    }, 
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1,
+        maxLength: 20
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1,
+        maxLength: 20
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
