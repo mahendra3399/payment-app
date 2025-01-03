bc670acda2e20194036f20f9e53736659071a369
mahendra <pvdmahendra@gmail.com>
2025-01-03 23:58:51 +0530

    created user route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import router from "./routes/index.js";
+import userRouter from "./routes/user.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use("/api/v1", router);
+app.use("/api/v1/user", userRouter);
 
 const PORT = process.env.PORT || 5000;
 
