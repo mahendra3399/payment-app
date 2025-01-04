c0f8e62986f720bd3bc577b48aff6738c1ec8170
mahendra <pvdmahendra@gmail.com>
2025-01-04 17:42:04 +0530

    added CORS support, updated port, and implemented JWT secret configuration

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "mahendra";
+
+export { JWT_SECRET };
--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import router from "./routes/index.js";
-import userRouter from "./routes/user.js";
+import cors from "cors";
 
 const app = express();
 
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1", router);
-app.use("/api/v1/user", userRouter);
 
 const PORT = process.env.PORT || 5000;
 
