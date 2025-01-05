8815e37c53f83f77402823492e5f8fa5135c2701
mahendra <pvdmahendra@gmail.com>
2025-01-06 02:27:55 +0530

    added authentication middleware

---
+++ backend/middleware.js
@@ -0,0 +1,23 @@
+import {JWT_SECRET} from './config';
+import jwt from 'jsonwebtoken';
+
+export const authMiddleware = (req,res,next) => {
+    const authHeader = req.header.authrization;
+    if(!authHeader || !authHeader.startsWith('Bearer')){
+        res.status(403).send('Unauthorized');
+    }
+    
+    const token = authHeader.split(' ')[1];
+    try {
+       const decoded = jwt.verify(token,JWT_SECRET);
+       if(decoded){
+           req.userId = decoded.userId;
+           next();
+       }
+       else{
+          return res.status(403).send('Unauthorized');
+       }
+    } catch (error) {
+        return res.status(403).send('Unauthorized');
+    }
+}
