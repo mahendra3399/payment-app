daf9e367096e9e09ffd7ba992da550e4606616e8
mahendra <pvdmahendra@gmail.com>
2025-01-07 03:55:09 +0530

    added bulk user retrieval route with filtering

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -79,4 +79,29 @@ userRouter.put("/", authMiddleware, async (req, res) => {
     res.status(200).json({ message: "User updated successfully"});
 })
 
+userRouter.get("/bulk", async (req,res)=> {
+    const filter = req.query.filter || {};
+    
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+
 export default userRouter;
