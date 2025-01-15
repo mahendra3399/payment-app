d9ef454dcb256dedc083135a194795c9c62b1ca6
mahendra <pvdmahendra@gmail.com>
2025-01-15 18:32:28 +0530

    improved SendMoney component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send-money" element={<SendMoney />} />
+          <Route path="/send" element={<SendMoney />} />
         </Routes>
       </BrowserRouter>
     </>
